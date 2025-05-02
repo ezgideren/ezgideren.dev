c0e5ecd63b140dc1e864829abf4e4cdb841613dc
Ezgi Deren <ezgi_deren@yahoo.com>
2025-05-02 11:07:29 +0200

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,15 @@ server.listen(3000, function () {
   console.log("server started on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /* Begin Websocket */
 
 const WebSocketServer = require("ws").Server;
@@ -26,6 +35,11 @@ wss.on("connection", function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send("Welcome to my server");
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients} datetime('now'))
+    `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log("A client has disconnected");
@@ -36,3 +50,32 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/* End Websockets */
+
+/* Begin Database */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  //making sure the table is set up
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT 
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down db");
+  db.close;
+}
